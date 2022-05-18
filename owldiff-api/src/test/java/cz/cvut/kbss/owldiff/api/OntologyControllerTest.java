814710c2ac4875ef3b40e22238f08016317eff96
Vévar, Lukáš <vevarluk@fel.cvut.cz>
2022-05-18 21:01:40 +0200

    Fixed test

--- owldiff-api/src/test/java/cz/cvut/kbss/owldiff/api/OntologyControllerTest.java
+++ owldiff-api/src/test/java/cz/cvut/kbss/owldiff/api/OntologyControllerTest.java
@@ -1,5 +1,6 @@
 package cz.cvut.kbss.owldiff.api;
 
+import cz.cvut.kbss.owldiff.api.config.HttpSessionConfig;
 import cz.cvut.kbss.owldiff.api.rest.OntologyController;
 import cz.cvut.kbss.owldiff.api.service.OntologyService;
 
@@ -23,7 +24,11 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionContext;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebMvcTest(OntologyController.class)
 public class OntologyControllerTest {
@@ -31,13 +36,103 @@ public class OntologyControllerTest {
     @MockBean
     private OntologyService serviceMocked;
 
+    @MockBean
+    private HttpSessionConfig httpSessionConfig;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void getOntologiesById_whenValid_thenReturns200AndResponse() throws Exception {
+        HttpSession tmp = new HttpSession() {
+            @Override
+            public long getCreationTime() {
+                return 0;
+            }
+
+            @Override
+            public String getId() {
+                return null;
+            }
+
+            @Override
+            public long getLastAccessedTime() {
+                return 0;
+            }
+
+            @Override
+            public ServletContext getServletContext() {
+                return null;
+            }
+
+            @Override
+            public void setMaxInactiveInterval(int interval) {
+
+            }
+
+            @Override
+            public int getMaxInactiveInterval() {
+                return 0;
+            }
+
+            @Override
+            public HttpSessionContext getSessionContext() {
+                return null;
+            }
+
+            @Override
+            public Object getAttribute(String name) {
+                return null;
+            }
+
+            @Override
+            public Object getValue(String name) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getAttributeNames() {
+                return null;
+            }
+
+            @Override
+            public String[] getValueNames() {
+                return new String[0];
+            }
+
+            @Override
+            public void setAttribute(String name, Object value) {
+
+            }
+
+            @Override
+            public void putValue(String name, Object value) {
+
+            }
+
+            @Override
+            public void removeAttribute(String name) {
+
+            }
+
+            @Override
+            public void removeValue(String name) {
+
+            }
+
+            @Override
+            public void invalidate() {
+
+            }
+
+            @Override
+            public boolean isNew() {
+                return false;
+            }
+        };
         // when
-        when(serviceMocked.getOntologiesById(any())).thenReturn(new ResponseEntity<>("Saved ontologies", HttpStatus.OK));
+        when(httpSessionConfig.getSessionById(any())).thenReturn(tmp);
+        when(serviceMocked.getOntologiesById(tmp)).thenReturn(new ResponseEntity<>("Saved ontologies", HttpStatus.OK));
 
         // mock and assert
         mockMvc.perform(get("/api/ontology/upload/test")).andDo(print()).andExpect(status().isOk())
